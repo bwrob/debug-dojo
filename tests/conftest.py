b9ed365b08fbb705785bfda334f2f738b416cc3d
bwrob <bartosz.marcin.wroblewski@gmail.com>
2026-01-31 22:16:52 +0100

    infra: cleanup and complexipy

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,18 +25,6 @@ def test_target_inspect() -> str:
 
 
 @pytest.fixture
-def test_target_exception() -> str:
-    """Provide a path to a test target file for exception testing."""
-    return "tests/assets/main_exception.py"
-
-
-@pytest.fixture
-def test_target_breakpoint() -> str:
-    """Provide a path to a test target file for breakpoint testing."""
-    return "tests/assets/main_breakpoint.py"
-
-
-@pytest.fixture
 def expected_dict_output() -> str:
     """Provide the expected output of the example_dict inspection."""
     return "{'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}"
@@ -46,15 +34,3 @@ def expected_dict_output() -> str:
 def runner() -> CliRunner:
     """Provide a Typer CLI runner fixture."""
     return CliRunner()
-
-
-@pytest.fixture
-def test_string() -> str:
-    """Provide a test string."""
-    return "This is a test string."
-
-
-@pytest.fixture
-def another_test_string() -> str:
-    """Provide another test string."""
-    return "This is another test string."
